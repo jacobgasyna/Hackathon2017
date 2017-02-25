a1709f809eeabdbc94ec48aaa9a6b2dfdd0deb42
¨Jacob <jacob.gasyna@gmail.com>
2017-02-25 13:50:19 -0600

    start,pause,start

--- Icons/basicspython3.py
+++ Icons/basicspython3.py
@@ -38,7 +38,7 @@ hnPath = 'HalfNoteicon.png'
 hnImage = Image.open(hnPath)
 hn = ImageTk.PhotoImage(hnImage)
 hnButton = Button(sidebar, image=hn, border=0)
-hnButton.place(x=25, y=0)
+hnButton.place(x=25, y=75)
 
 hnRestPath = 'HalfResticon.png'   
 hnRestImage = Image.open(hnRestPath)
@@ -50,7 +50,7 @@ qnPath = 'QuarterNoteicon.png'
 qnImage = Image.open(qnPath)
 qn = ImageTk.PhotoImage(qnImage)
 qnButton = Button(sidebar, image=qn, border=0)
-qnButton.place(x=25, y=75)
+qnButton.place(x=25, y=150)
 
 qnRestPath = 'QuarterResticon.png'   
 qnRestImage = Image.open(qnRestPath)
@@ -62,57 +62,61 @@ enPath = 'EigthNoteicon.png'
 enImage = Image.open(enPath)
 en = ImageTk.PhotoImage(enImage)
 enButton = Button(sidebar, image=en, border=0)
-enButton.place(x=25, y=150)
+enButton.place(x=25, y=225)
 
 enRestPath = 'EigthResticon.png'   
 enRestImage = Image.open(enRestPath)
 enRest = ImageTk.PhotoImage(enRestImage)
 enRestButton = Button(sidebar, image=enRest, border=0)
-enRestButton.place(x=100, y=150)
+enRestButton.place(x=100, y=225)
 
 snPath = 'SixteenthNoteicon.png'
 snImage = Image.open(snPath)
 sn = ImageTk.PhotoImage(snImage)
 snButton = Button(sidebar, image=sn, border=0)
-snButton.place(x=25, y=225)
+snButton.place(x=25, y=300)
 
 snRestPath = 'SixteenthResticon.png'   
 snRestImage = Image.open(snRestPath)
 snRest = ImageTk.PhotoImage(snRestImage)
 snRestButton = Button(sidebar, image=snRest, border=0)
-snRestButton.place(x=100, y=225)
+snRestButton.place(x=100, y=300)
 
 tnPath = 'ThirtysecondNoteicon.png'
 tnImage = Image.open(tnPath)
 tn = ImageTk.PhotoImage(tnImage)
 tnButton = Button(sidebar,image=tn, border=0)
-tnButton.place(x=25, y=300)
+tnButton.place(x=25, y=375)
 
 tnRestPath = 'ThirtysecondResticon.png'   
 tnRestImage = Image.open(tnRestPath)
 tnRest = ImageTk.PhotoImage(tnRestImage)
 tnRestButton = Button(sidebar, image=tnRest, border=0)
-tnRestButton.place(x=100, y=300)
+tnRestButton.place(x=100, y=375)
 
-startbpath = ''   
-startbImage = Image.open(startpathPath)
+startbPath = 'PlayIcon.png'   
+startbImage = Image.open(startbPath)
 startb = ImageTk.PhotoImage(startbImage)
 startButton = Button(sidebar, image=startb, border=0)
-startButton.place(x=60, y=375)
+startButton.place(x=60, y=440)
 
-pausebpath = ''
+pausebPath = 'PauseIcon.png'
 pausebImage = Image.open(pausebPath)
 pauseb = ImageTk.PhotoImage(pausebImage)
 pauseButton = Button(sidebar, image=pauseb, border=0)
-pauseButton.place(x=60, y=450)
+pauseButton.place(x=60, y=495)
 
-stopbpath = ''   
+stopbPath = 'StopIcon.png'   
 stopbImage = Image.open(stopbPath)
 stopb = ImageTk.PhotoImage(stopbImage)
 stopButton = Button(sidebar, image=stopb, border=0)
-stopButton.place(x=60, y=525)
+stopButton.place(x=60, y=545)
 
-
+delbPath = 'Trash.png'   
+delbImage = Image.open(delbPath)
+delb = ImageTk.PhotoImage(delbImage)
+delButton = Button(sidebar, image=delb, border=0)
+delButton.place(x=68, y=605)
 
 
 
