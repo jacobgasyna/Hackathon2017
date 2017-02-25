123bba23fac78a995a2278d0538de8898547c0e7
¨Jacob <jacob.gasyna@gmail.com>
2017-02-25 10:03:09 -0600

    ¨classes¨

---
+++ NoteImagesAndClasses/MusicNoteWebsite.py
@@ -0,0 +1,69 @@
+import Image
+
+class Noteclass():
+    Note=0
+
+class Wholenote(Noteclass):
+    len_Wholenote = 32
+    img_Wholenote = 
+        
+
+class Halfnote(Noteclass):
+    len_halfnote = 16
+    img_halfnote = 
+        
+
+class Quarternote(Noteclass):
+    len_Quarternote = 8
+    img_quarternote = 
+        
+
+class Eigthnote(Noteclass):
+    len_Eighthnote = 4
+    img_Eighthnote = 
+        
+
+class Sixteenthnote(Noteclass):
+    len_Sixteenthnote = 2
+    img_Sixteenthnote = 
+        
+
+class Thirtysecondnote(Noteclass):
+    len_Thirtysecondnote = 1
+    img_Thirtysecondnote =
+
+class Wholerest(Noteclass):
+    len_wholerest = 32
+    img_wholerest = 
+    
+
+class Halfrest(Noteclass):
+    len_halfrest = 16
+    img_halfrest = 
+    
+    
+
+class Quarterrest(Noteclass):
+    len_Quarterrest = 8
+    len_Quarterrest = 
+    
+
+class Eigthrest(Noteclass):
+    len_Eightrest = 4
+    img_Eighthrest = 
+    
+
+class Sixteenth(Noteclass):
+    len_Sixteenth = 2
+    img_Sixteenth = 
+    
+
+class Thirtysecondrest(Noteclass):
+    len_Thirtysecondrest = 1
+    img_Thirtysecondrest = 
+        
+
+
+    
+        
+
