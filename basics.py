a5d4df10cc6eab9e112b9f950e70fa667b7d0994
jrogge <jwrogge@live.com>
2017-02-25 11:54:02 -0600

    Add files via upload

--- basics.py
+++ basics.py
@@ -4,15 +4,14 @@ from PIL import Image, ImageTk
 def commandTest():
     print "test"
 
-def makeButtons(root)
+#def makeButtons(sidebar):
+    #""" place all the buttons in the sidebar"""
+    #TODO add commands for each button
+
 root = Tk()
 screenWidth = root.winfo_screenwidth()
 screenHeight = root.winfo_screenheight()
 root.geometry('%dx%d+%d+%d' % (screenWidth, screenHeight, 0, 0))
-
-filePath = 'QuarterNoteicon.png'
-iconImage = Image.open(filePath)
-icon = ImageTk.PhotoImage(iconImage)
 
 # main area
 mainarea = Frame(root, bg='#FFA', width=screenWidth-200)
@@ -22,8 +21,31 @@ mainarea.pack(expand=True, fill='both', side='right')
 sidebar = Frame(root, bg='#FFF', width=200)
 sidebar.pack(expand=False, fill='both', side='left', anchor='nw')
 
-testButton = Button(sidebar, image=icon, command=commandTest)
-testButton.place(x=25,y=0)
+# make buttons
+qnPath = 'QuarterNoteicon.png'
+qnImage = Image.open(qnPath)
+qn = ImageTk.PhotoImage(qnImage)
+qnButton = Button(sidebar, image=qn, border=0)
+qnButton.place(x=25, y=0)
+
+
+qnRestPath = 'QuarterResticon.png'   
+qnRestImage = Image.open(qnRestPath)
+qnRest = ImageTk.PhotoImage(qnRestImage)
+qnRestButton = Button(sidebar, image=qnRest, border=0)
+qnRestButton.place(x=100, y=0)
+
+wnPath = 'WholeNoteicon.png'
+wnImage = Image.open(wnPath)
+wn = ImageTk.PhotoImage(wnImage)
+wnButton = Button(sidebar, image=wn, border=0)
+wnButton.place(x=25, y=75)
+
+
+wnRestPath = 'WholeResticon.png'   
+wnRestImage = Image.open(wnRestPath)
+wnRest = ImageTk.PhotoImage(wnRestImage)
+wnRestButton = Button(sidebar, image=wnRest, border=0)
+wnRestButton.place(x=100, y=75)
 
 root.mainloop()
-
